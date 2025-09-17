91293f9d44732b2d86d048553da6074447198d0d
HTY725 <72512031@cityu-dg.edu.cn>
2025-09-17 11:33:08 +0800

    Update week3-mcr.py

--- week3-mcr.py
+++ week3-mcr.py
@@ -2,7 +2,7 @@
 # Reviewer 1:Xinyi Zhang(name), 72512043(ID), koko-evan(GitHub name)
 # Reviewer 2:Xuecheng Huang(name), 72510406(ID), Mucheng66(GitHub name)
 # Reviewer 3:Jingwen Jia(name), 72512092(ID), Lixibeijjw(GitHub name)
-# Reviewer 4:Liang yue(name), 72512031(ID), HTY725(GitHub name)
+# Reviewer 4:Liang yue岳亮(name), 72512031(ID), HTY725(GitHub name)
 # Reviewer 5:Jiacheng Qian(name), 72510756(ID), qianjiachen(GitHub name)
 
 def is_win(game):
